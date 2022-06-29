95b793f887cfe8ea2e1bdf6d9e79d8f268cd9263
douglasribeiro <dougas.ribeiro.id@gmail.com>
2022-06-29 17:37:18 -0300

    Autorizacao com JWT

--- src/main/java/com/douglas/helpdesk/resource/TecnicoResource.java
+++ src/main/java/com/douglas/helpdesk/resource/TecnicoResource.java
@@ -8,9 +8,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,29 +23,47 @@ import com.douglas.helpdesk.domain.Tecnico;
 import com.douglas.helpdesk.domain.dto.TecnicoDTO;
 import com.douglas.helpdesk.service.TecnicoService;
 
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-	
+
 	@Autowired
-	private TecnicoService tecnicoService;
+	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
-		return ResponseEntity.ok().body(new TecnicoDTO(tecnicoService.findById(id)));
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
+		Tecnico obj = service.findById(id);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<TecnicoDTO>> listAll(){
-		return ResponseEntity.ok()
-				.body(tecnicoService.findAll().stream().map(x -> new TecnicoDTO(x)).collect(Collectors.toList()));
+	public ResponseEntity<List<TecnicoDTO>> findAll() {
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
-	public ResponseEntity<TecnicoDTO> newTecnico(@Valid @RequestBody TecnicoDTO tecnicoDTO){
-		Tecnico newTecnico = tecnicoService.save(tecnicoDTO);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newTecnico.getId()).toUri();
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @Valid @RequestBody TecnicoDTO objDTO) {
+		Tecnico obj = service.update(id, objDTO);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id) {
+		service.delete(id); 
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/douglas/helpdesk/service/TecnicoService.java
+++ src/main/java/com/douglas/helpdesk/service/TecnicoService.java
@@ -3,6 +3,8 @@ package com.douglas.helpdesk.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,44 +17,66 @@ import com.douglas.helpdesk.repository.TecnicoRepository;
 import com.douglas.helpdesk.service.exception.DataIntegrityViolationException;
 import com.douglas.helpdesk.service.exception.ObjectNotFoundException;
 
+
 @Service
 public class TecnicoService {
 
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	
+	private TecnicoRepository repository;
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
 	@Autowired
 	private BCryptPasswordEncoder encoder;
-	
+
 	public Tecnico findById(Integer id) {
-		Optional<Tecnico> obj = tecnicoRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado!"));
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
 	public List<Tecnico> findAll() {
-		return tecnicoRepository.findAll();
+		return repository.findAll();
 	}
 
-	public Tecnico save(TecnicoDTO tecnicoDTO) {
-		Tecnico obj = new Tecnico(tecnicoDTO);
-		obj.setSenha(encoder.encode(tecnicoDTO.getSenha()));
-		validaCpfEmail(tecnicoDTO);
-		obj.setId(null);
-		return tecnicoRepository.save(obj);
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		validaPorCpfEEmail(objDTO);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
-
-	private void validaCpfEmail(TecnicoDTO tecnicoDTO) {
-		Optional<Pessoa> obj = pessoaRepository.findByCpf(tecnicoDTO.getCpf());
-		if (obj.isPresent() && obj.get().getId() != tecnicoDTO.getId()) {
-			throw new DataIntegrityViolationException("C.P.F ja cadastrado.");
-		}
+ 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
 		
-		 obj = pessoaRepository.findByEmail(tecnicoDTO.getEmail());
-		if (obj.isPresent() && obj.get().getId() != tecnicoDTO.getId()) {
-			throw new DataIntegrityViolationException("E-mail ja cadastrado.");
+		if(!objDTO.getSenha().equals(oldObj.getSenha())) 
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+
+		repository.deleteById(id);
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
+
 }
