44d1f15cfed0f70f38de9cc648ea5eca1b94475a
douglasribeiro <dougas.ribeiro.id@gmail.com>
2022-07-04 10:21:24 -0300

    Clientes

--- src/main/java/com/douglas/helpdesk/service/ClienteService.java
+++ src/main/java/com/douglas/helpdesk/service/ClienteService.java
@@ -3,13 +3,15 @@ package com.douglas.helpdesk.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.douglas.helpdesk.domain.Cliente;
 import com.douglas.helpdesk.domain.Pessoa;
 import com.douglas.helpdesk.domain.dto.ClienteDTO;
-import com.douglas.helpdesk.domain.Cliente;
 import com.douglas.helpdesk.repository.ClienteRepository;
 import com.douglas.helpdesk.repository.PessoaRepository;
 import com.douglas.helpdesk.service.exception.DataIntegrityViolationException;
@@ -36,7 +38,7 @@ public class ClienteService {
 		return clienteRepository.findAll();
 	}
 
-	public Cliente save(ClienteDTO clienteDTO) {
+	public Cliente create(ClienteDTO clienteDTO) {
 		Cliente obj = new Cliente(clienteDTO);
 		obj.setSenha(encoder.encode(clienteDTO.getSenha()));
 		validaCpfEmail(clienteDTO);
@@ -53,6 +55,40 @@ public class ClienteService {
 		 obj = pessoaRepository.findByEmail(clienteDTO.getEmail());
 		if (obj.isPresent() && obj.get().getId() != clienteDTO.getId()) {
 			throw new DataIntegrityViolationException("E-mail ja cadastrado.");
+		}
+	}
+	
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		objDTO.setId(id);
+		Cliente oldObj = findById(id);
+		
+		if(!objDTO.getSenha().equals(oldObj.getSenha())) 
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Cliente(objDTO);
+		return clienteRepository.save(oldObj);
+	}
+	
+	public void delete(Integer id) {
+		Cliente obj = findById(id);
+
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+		}
+
+		clienteRepository.deleteById(id);
+	}
+	
+	private void validaPorCpfEEmail(ClienteDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
 }
